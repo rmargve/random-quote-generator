240f3ec6515fe81716d15702bbaa96bc442565fd
Midnight <rmargve@gmail.com>
2019-12-06 01:45:24 +0400

    Revert "injecting mock data instead of useStaticQuery"
    This reverts commit 1eaa8d57f1334bd302958af30bdbf75a2dbfcf30.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,11 @@ import SEO from "../components/seo"
 import RandomQuote from "../components/randomQuoteGenerator"
 import "../styles/style.css"
 
-const IndexPage = () => {
-  console.log("hii")
-  return (
-    <div className="bg-teal-300 w-screen h-screen p-6">
-      <SEO />
-      <RandomQuote />
-    </div>
-  )
-}
+const IndexPage = () => (
+  <div className="bg-teal-300 w-screen h-screen p-6">
+    <SEO />
+    <RandomQuote />
+  </div>
+)
 
 export default IndexPage
