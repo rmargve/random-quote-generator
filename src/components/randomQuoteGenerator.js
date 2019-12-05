1eaa8d57f1334bd302958af30bdbf75a2dbfcf30
Midnight <rmargve@gmail.com>
2019-12-06 01:42:50 +0400

    injecting mock data instead of useStaticQuery

--- src/components/randomQuoteGenerator.js
+++ src/components/randomQuoteGenerator.js
@@ -4,24 +4,51 @@ import { useStaticQuery, graphql } from "gatsby"
 import "../styles/style.css"
 
 export default () => {
-  const data = useStaticQuery(graphql`
-    query fetchQuotes {
-      allDataJson {
-        edges {
-          node {
-            id
-            quotes {
-              quote
-              author
-            }
-          }
-        }
-      }
+  // const data = useStaticQuery(graphql`
+  //   query fetchQuotes {
+  //     allDataJson {
+  //       edges {
+  //         node {
+  //           id
+  //           quotes {
+  //             quote
+  //             author
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
+
+  // const quotes = data.allDataJson.edges[0].node.quotes
+
+  const quotes = [
+    {
+      "quote": "Life isn’t about getting and having, it’s about giving and being.",
+      "author": "Kevin Kruse"
+    },
+    {
+      "quote": "Whatever the mind of man can conceive and believe, it can achieve.",
+      "author": "Napoleon Hill"
+    },
+    {
+      "quote": "Strive not to be a success, but rather to be of value.",
+      "author": "Albert Einstein"
+    },
+    {
+      "quote": "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.",
+      "author": "Robert Frost"
+    },
+    {
+      "quote": "I attribute my success to this: I never gave or took any excuse.",
+      "author": "Florence Nightingale"
+    },
+    {
+      "quote": "You miss 100% of the shots you don’t take.",
+      "author": "Wayne Gretzky"
     }
-  `)
-
-  const quotes = data.allDataJson.edges[0].node.quotes
-
+  ]
+  
   const [randomQuote, setRandomQuote] = useState(() => {
     const initialQuote = generateQuote()
     return initialQuote
