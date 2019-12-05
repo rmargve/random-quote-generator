902a5586f23a1e5932af93e241083733939b39cd
Midnight <rmargve@gmail.com>
2019-12-06 01:46:35 +0400

    add logging

--- src/components/randomQuoteGenerator.js
+++ src/components/randomQuoteGenerator.js
@@ -21,11 +21,14 @@ export default () => {
   `)
 
   const quotes = data.allDataJson.edges[0].node.quotes
-
+    console.log('from outside')
   const [randomQuote, setRandomQuote] = useState(() => {
+    
     const initialQuote = generateQuote()
+    console.log('from inside', initialQuote)
     return initialQuote
   })
+  console.log('from outSide', randomQuote)
 
   function generateQuote() {
     return quotes[Math.floor(Math.random() * quotes.length)]
